b60578e1aaac56a306a385b5a000c3b965e87266
Fabio Navarro <80645214+Fabio-1984@users.noreply.github.com>
2024-04-25 17:33:25 -0300

    Update script.js
    Criando a função para validar os botões

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
+function envia(){
+ 
+   let nome = myForm.nome.value
+   let email = myForm.email.value
+   let telefone = myForm.telefone.value
+   let tipo_de_evento = myForm.tipoDeEvento.value
+   let data = myForm.data.value
+   let  mensagem = myForm.mensagem.value
+   
+   if (nome === ""){
+   
+     alert('Preencha o campo nome')
+     myForm.nome.focus()
+     return false
+   
+   }else if (email === ""){
+   
+     alert('Preencha o campo email')
+     myForm.email.focus()
+     return false
+   
+   }else if (telefone === ""){
+   
+     alert('Preencha o campo telefone')
+     myForm.htelefone.focus()
+     return false
+   
+   }else if (tipoDeEvento === ""){
+   
+     alert('Selecione um tipo de evento')
+     myForm.tipoCliente.focus()
+     return false
 
+   }else if (data === ""){
+   
+     alert('Selecione a data')
+     myForm.tipoCliente.focus()
+     return false
+
+   }else if (mensagem === ""){
+   
+     alert('Selecione a data')
+     myForm.tipoCliente.focus()
+     return false  
+   
+   }else {
+       alert('Dados enviados com sucesso. Em breve retornaremos')
+       document.forms["cadCli"].submit()
+       
+   }
+ 
+ }
 
 
 
@@ -21,26 +72,17 @@
 
 //Aqui esta a Validação de Formulário
 
-function validateForm(){
- var x = document.forms["myForm"]["nome","email","telefone","tipo de evento","data","mensagem"].value;
- if(x == ""){
-   alert("Dados não enviados verifique se não tem nenhum campo vazio, pois o preenchimento é obrigatório.");
-    return false;
+//function validateForm(){
+// var x = document.forms["myForm"]["nome","email","telefone","tipo de evento","data","mensagem"].value;
+// if(x == ""){
+ //  alert("Dados não enviados verifique se não tem nenhum campo vazio, pois o preenchimento é obrigatório.");
+   // return false;
    
- }else{
+ //}else{
 
-    alert('Dados enviados com sucesso. Entraremos em contato o mais breve possíve. Obrigado!')
- }
+  //  alert('Dados enviados com sucesso. Entraremos em contato o mais breve possíve. Obrigado!')
+// }
 
 
-}
-
-function openNav() {
-   document.getElementById("myMenus").style.height = "100%";
- }
- 
- 
- function closeNav() {
-   document.getElementById("myMenus").style.height = "0%";
- }
+//}
 
