de7eca7499c56fecf07f2d5acfc459c85f42126c
Fabio Navarro <80645214+Fabio-1984@users.noreply.github.com>
2024-04-25 21:36:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-o
 let formulario = document.querySelector('#myForm');
 formulario.addEventListener('submit', (evento) => {
 
-   let nome = document.querySelector('#nome');
-   let email = document.querySelector('#email');
-   let telefone = document.querySelector('#tel');
-   let tipo_de_evento = document.querySelector('#tp');
-   let data = document.querySelector('#data');
-   let  mensagem = document.querySelector('#text');
+   let nome = document.querySelector('#nome').value;
+   let email = document.querySelector('#email').value;
+   let telefone = document.querySelector('#tel').value;
+   let tipo_de_evento = document.querySelector('#tp').value;
+   let data = document.querySelector('#data').value;
+   let  mensagem = document.querySelector('#text').value;
 
 
   if (nome === ""){
